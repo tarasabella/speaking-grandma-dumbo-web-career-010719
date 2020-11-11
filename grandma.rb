05f02e4d887ebd849a54f3eda41336697a5880d5
Tara Sabella <tarasabella@gmail.com>
2020-11-11 05:37:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  if(phrase == "I LOVE YOU GRANDMA!")
     puts "I LOVE YOU TOO PUMPKIN!"
   # elsif response = response.upcase 
   #   puts "NO, NOT SINCE 1938!"
